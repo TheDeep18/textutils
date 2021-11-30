28323c27bfe76e18261f3f10c24549a610f4bb39
TheDeep <theDeep1801@gmail.com>
2021-11-30 16:30:03 +0530

    end of video 10

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -14,15 +14,33 @@ export default function TextForm(props) {
         let newText = text.toLowerCase();
         setText(newText) 
     }
+    const handleClearClick = () => {
+        // console.log('Uppercase was clicked ' + text);
+        let newText = "";
+        setText(newText) 
+    }
 
     const handleOnChange = (event) => {
         // console.log('Handleon change');
         setText(event.target.value)  
     }
 
-    const onClear = () => {
-        // console.log('Textarea was clicked');
-        setText("")
+    // const onClear = () => {
+    //     // console.log('Textarea was clicked');
+    //     setText("")
+    // }
+
+    const handlecopy = () => {
+        console.log('I am copy');
+        var text = document.getElementById("myBox")
+        text.select();
+        navigator.clipboard.writeText(text.value);
+        
+    }
+
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "))
     }
 
     const [text, setText] = useState('');
@@ -33,10 +51,13 @@ export default function TextForm(props) {
         <div className="container">
             <div>
                 <h1>{props.heading}</h1>
-                <textarea className="form-control" id="myBox" cols="30" rows="10" value={text} onClick={onClear} onChange={handleOnChange}></textarea>
+                <textarea className="form-control" id="myBox" cols="30" rows="10" value={text} onChange={handleOnChange}></textarea>
             </div>
             <button className="btn btn-primary my-3" onClick={handleUpClick}>Convert to uppercase</button>
             <button className="btn btn-primary my-3 mx-2" onClick={handleLowClick}>Convert to lowercase</button>
+            <button className="btn btn-primary my-3 mx-2" onClick={handleClearClick}>Clear</button>
+            <button className="btn btn-primary my-3 mx-2" onClick={handlecopy}>Copy text</button>
+                <button className="btn btn-primary my-3 mx-2" onClick={handleExtraSpaces}>Remove extra spaces</button>
         </div>
 
         <div className="container">
